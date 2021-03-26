edb5d6d4ff13ffe6810d7fc8ff466010e5b40c82
Nick Roma <nikoandpiko@gmail.com>
2021-03-26 11:59:15 +0900

    Changed server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://boiling-journey-21552.herokuapp.com/",
+  uri: "https://floating-hamlet-98872.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
